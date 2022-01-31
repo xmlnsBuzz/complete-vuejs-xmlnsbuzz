0290690158a5c6226e377eb438d7d1060a3011ea
xmlnsBuzz <xmlnxbuzz@gmail.com>
2022-01-31 22:17:01 +0900

    02. isEven을 사용한 v-if, v-else. 그리고 생략 가능한 것들

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import * as Vue from 'vue/dist/vue.esm-bundler.js';
 
 const app = Vue.createApp( {
   template: `
-    <button v-on:click="increment()">Increment 1</button>
-    <button v-on:click="increment">Increment 함수 뒤에 괄호 없어도 됨</button>
-    
-    <p>{{count}} </p>
+    <button v-on:click="increment()">Increment</button>
+    <p>{{count}}</p>
+    <div v-if="isEven()">
+      Even
+    </div>
+    <div v-else>
+      Odd
+    </div>
   `,
   /* NOTE increment()과 increment는 똑 같음. (괄호)를 생략할 수 있음 */
   data () {
@@ -17,7 +21,13 @@ const app = Vue.createApp( {
   methods: {
     increment () {
       this.count += 1;
+    },
+    isEven () {
+      return this.count % 2 === 0
     }
+    /* isEven ( val ) {
+      return val % 2 === 0
+    } */
   }
 } );
 app.mount( '#app' );
---
+++ vue3-배우는 순서.md
@@ -0,0 +1,10 @@
+# Vue 3 학습 순서 및 내용
+
+## 함수 뒤의 괄호 생략 가능함
+```html
+ <button v-on:click="increment()">Increment 1</button>
+ <button v-on:click="increment">Increment 함수 뒤에 괄호 없어도 됨</button>
+ ```
+2 가지가 똑 같음
+
+## v-if, v-else
