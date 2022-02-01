7c668524e3e7e64f63fde4ce25fcc6fb6f6d68ad
xmlnsBuzz <xmlnxbuzz@gmail.com>
2022-02-01 22:19:09 +0900

    02.04 Custom input foundation. v-bind: -> :로 생략 가능

--- form-validation/App.vue
+++ form-validation/App.vue
@@ -1,12 +1,16 @@
 <template>
+  <my-input name="Username" />
   <my-button background="darkslateblue" color="white" :disabled="!valid" />
 </template>
 
 <script>
   import MyButton from './MyButton.vue'
+  import MyInput from './MyInput.vue'
+
   export default {
     components: {
-      MyButton
+      MyButton,
+      MyInput
     },
 
     data(){
---
+++ form-validation/MyInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="label" >
+    <label :for="name">{{name}} </label>
+  </div>
+  <input :id="name" />
+  <!-- SPEC v-bind:는 :로 생략 가능 -->
+</template>
+
+<script>
+  export default {
+    props: {
+name:{
+  type: String,
+  required: true
+}
+    }
+  }
+</script>
+
+<style scoped>
+  .input-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .error {
+    color: #f00;
+  }
+
+  label {
+    cursor: pointer;
+  }
+
+  .label {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  input {
+    background-color: none;
+    color: #000;
+    border: 1px solid silver;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 5px 0;
+    font-size: 16px;
+  }
+</style>
