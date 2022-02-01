cbe85f53b18f058e016baa26ae29c1d55fab3bfb
xmlnsBuzz <xmlnxbuzz@gmail.com>
2022-02-01 18:27:46 +0900

    01.14-1 :click이 빠진 상태에서는 button클릭해도 숫자 안 찍힘.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import * as Vue from 'vue/dist/vue.esm-bundler.js';
 const Num = {
   props: [ 'number' ],
   template: `
-  <div
+  <button
     v-bind:class="getClass(number)"
+    v-on="click(number)"
   >
     {{ number }}
-  </div>
+  </button>
 `,
   /* NOTE component를 reuse하기 위해서는 methods를 component 내부에 놓아야 된다.
   아래의 getClass, isEven을 최상위 methods에서 Num component 내부로 옮겼다. */
   methods: {
+    click (number) {
+      console.log(number)
+    },
     getClass ( number ) {
       return this.isEven( number ) ? 'blue' : 'red';
     },
@@ -28,27 +32,6 @@ const app = Vue.createApp( {
   },
 
   template: `
-    <button v-on:click="increment()">Increment</button>
-    <p>{{count}}</p>
-
-    <input 
-     type="checkbox"
-     v-model="value" 
-     value="a"
-    />
-    <input 
-     type="checkbox"
-     v-model="value"
-     value="b"
-    />
-
-    {{ value }}
-
-    <div class="red">
-      {{ error }}
-    </div>
-    
-    
     <num 
       v-for="number in numbers"
       v-bind:number="number" 
@@ -58,8 +41,6 @@ const app = Vue.createApp( {
 
   data () {
     return {
-      count: 0,
-      value: [ 'a' ],
       numbers: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
     };
   },
@@ -70,17 +51,9 @@ const app = Vue.createApp( {
     evenList () {
       return this.numbers.filter( num => this.isEven( num ) );
     },
-    error () {
-      if ( this.value.length < 5 ) {
-        return 'Must be greater than 5.';
-      }
-    }
   },
 
   methods: {
-    increment () {
-      this.count += 1;
-    },
   },
 
 } );
